31ed137a03b702be141195680c4a821945824397
Chris DeRose <cderose@derosetechnologies.com>
2009-05-23 16:44:28 -0400

    Playing with the repo - manually updating from prior svn

--- Rakefile
+++ Rakefile
@@ -1,10 +1,11 @@
-# Add your own tasks in files placed in lib/tasks ending in .rake,
-# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
-require(File.join(File.dirname(__FILE__), 'config', 'boot'))
+require File.join(File.dirname(__FILE__), 'config', 'boot')
 
 require 'rake'
 require 'rake/testtask'
 require 'rake/rdoctask'
 
 require 'tasks/rails'
+
+unless Rake::Task.task_defined? "brisk_bills:release"
+  Dir["#{BRISKBILLS_ROOT}/lib/tasks/**/*.rake"].sort.each { |ext| load ext }
+end
--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,17 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
-RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
+RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/..") unless defined?(RAILS_ROOT)
 
 module Rails
+  class << self
+    def vendor_rails?
+      File.exist?("#{RAILS_ROOT}/vendor/rails")
+    end
+  end
+end
+
+module BriskBills
   class << self
     def boot!
       unless booted?
@@ -13,19 +21,34 @@ module Rails
     end
 
     def booted?
-      defined? Rails::Initializer
+      defined? BriskBills::Initializer
     end
 
     def pick_boot
-      (vendor_rails? ? VendorBoot : GemBoot).new
+      case
+      when app?
+        AppBoot.new
+      when vendor?
+        VendorBoot.new
+      else
+        GemBoot.new
+      end
     end
 
-    def vendor_rails?
-      File.exist?("#{RAILS_ROOT}/vendor/rails")
+    def vendor?
+      File.exist?("#{RAILS_ROOT}/vendor/brisk-bills")
+    end
+    
+    def app?
+      File.exist?("#{RAILS_ROOT}/lib/brisk-bills.rb")
     end
 
     def preinitialize
       load(preinitializer_path) if File.exist?(preinitializer_path)
+    end
+    
+    def loaded_via_gem?
+      pick_boot.is_a? GemBoot
     end
 
     def preinitializer_path
@@ -36,46 +59,74 @@ module Rails
   class Boot
     def run
       load_initializer
-      Rails::Initializer.run(:set_load_path)
+    end
+    
+    def load_initializer
+      begin
+        require 'brisk-bills'
+        require 'brisk-bills/initializer'
+      rescue LoadError => e
+        $stderr.puts %(BriskBills could not be initialized. #{load_error_message})
+        exit 1
+      end
+      BriskBills::Initializer.run(:set_load_path)
     end
   end
 
   class VendorBoot < Boot
     def load_initializer
-      require "#{RAILS_ROOT}/vendor/rails/railties/lib/initializer"
-      Rails::Initializer.run(:install_gem_spec_stubs)
-      Rails::GemDependency.add_frozen_gem_path
+      $LOAD_PATH.unshift "#{RAILS_ROOT}/vendor/brisk-bills/lib" 
+      super
+    end
+        
+    def load_error_message
+      "Please verify that vendor/brisk-bills contains a complete copy of the BriskBills sources."
+    end
+  end
+
+  class AppBoot < Boot
+    def load_initializer
+      $LOAD_PATH.unshift "#{RAILS_ROOT}/lib" 
+      super
+    end
+
+    def load_error_message
+      "Please verify that you have a complete copy of the BriskBills sources."
     end
   end
 
   class GemBoot < Boot
     def load_initializer
       self.class.load_rubygems
-      load_rails_gem
-      require 'initializer'
+      load_briskbills_gem
+      super
+    end
+      
+    def load_error_message
+     "Please reinstall the BriskBills gem with the command 'gem install brisk-bills'."
     end
 
-    def load_rails_gem
+    def load_briskbills_gem
       if version = self.class.gem_version
-        gem 'rails', version
+        gem 'brisk-bills', version
       else
-        gem 'rails'
+        gem 'brisk-bills'
       end
     rescue Gem::LoadError => load_error
-      $stderr.puts %(Missing the Rails #{version} gem. Please `gem install -v=#{version} rails`, update your RAILS_GEM_VERSION setting in config/environment.rb for the Rails version you do have installed, or comment out RAILS_GEM_VERSION to use the latest version installed.)
+      $stderr.puts %(Missing the BriskBills #{version} gem. Please `gem install -v=#{version} rails`, update your BRISKBILLS_GEM_VERSION setting in config/environment.rb for the Rails version you do have installed, or comment out BRISKBILLS_GEM_VERSION to use the latest version installed.)
       exit 1
     end
 
     class << self
       def rubygems_version
-        Gem::RubyGemsVersion rescue nil
+        Gem::RubyGemsVersion if defined? Gem::RubyGemsVersion
       end
 
       def gem_version
-        if defined? RAILS_GEM_VERSION
-          RAILS_GEM_VERSION
-        elsif ENV.include?('RAILS_GEM_VERSION')
-          ENV['RAILS_GEM_VERSION']
+        if defined? BRISKBILLS_GEM_VERSION
+          BRISKBILLS_GEM_VERSION
+        elsif ENV.include?('BRISKBILLS_GEM_VERSION')
+          ENV['BRISKBILLS_GEM_VERSION']
         else
           parse_gem_version(read_environment_rb)
         end
@@ -83,19 +134,19 @@ module Rails
 
       def load_rubygems
         require 'rubygems'
-        min_version = '1.3.1'
-        unless rubygems_version >= min_version
-          $stderr.puts %Q(Rails requires RubyGems >= #{min_version} (you have #{rubygems_version}). Please `gem update --system` and try again.)
+
+        unless rubygems_version >= '0.9.4'
+          $stderr.puts %(BriskBills requires RubyGems >= 0.9.4 (you have #{rubygems_version}). Please `gem update --system` and try again.)
           exit 1
         end
 
       rescue LoadError
-        $stderr.puts %Q(Rails requires RubyGems >= #{min_version}. Please install RubyGems and try again: http://rubygems.rubyforge.org)
+        $stderr.puts %(BriskBills requires RubyGems >= 0.9.4. Please install RubyGems and try again: http://rubygems.rubyforge.org)
         exit 1
       end
 
       def parse_gem_version(text)
-        $1 if text =~ /^[^#]*RAILS_GEM_VERSION\s*=\s*["']([!~<>=]*\s*[\d.]+)["']/
+        $1 if text =~ /^[^#]*BRISKBILLS_GEM_VERSION\s*=\s*["']([!~<>=]*\s*[\d.]+)["']/
       end
 
       private
@@ -107,4 +158,4 @@ module Rails
 end
 
 # All that for this:
-Rails.boot!
+BriskBills.boot!
